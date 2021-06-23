6dc86fb8bd801f4b10e108366b32fd2194a24665
shin224224 <sasasaeshin@gmail.com>
2021-06-23 21:09:09 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  #rails g contorller items indexででてくる
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "items#index"
+  #TOPページを表示させる。
 end
