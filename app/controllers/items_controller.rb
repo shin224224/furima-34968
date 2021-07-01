7b9e72ed11a93974bcfff8e2c574b8d68948ac30
shin224224 <sasasaeshin@gmail.com>
2021-07-01 23:25:50 +0900

    画面移動の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,29 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+  
+  
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+      @item = Item.new(item_params)
+       if @item.save
+        redirect_to root_path
+         
+       else
+         render :new
+       end
+    
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :category_id, :explanation, :status_id, :delivery_charge_id, :prefecture_id, :scheduled_day_id, :price, :user ).merge(user_id: current_user.id)
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  resources :items, only: [:index, :new, :create]
+
+ 
   get 'items/index'
   #rails g contorller items indexででてくる
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
   #TOPページを表示させる。
+  #activehachのルーティング
+
 end
