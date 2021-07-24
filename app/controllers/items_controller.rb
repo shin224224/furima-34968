a99ca70450dbc135a0e6ec7906fbbec7899ee200
shin224224 <sasasaeshin@gmail.com>
2021-07-24 15:27:08 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show, :new, :create]
+  before_action :move_to_index, except: [:index, :new, :create]
 
   # usercontroller はユーザの詳細ページがいる場合は作成する必要がある。
 
