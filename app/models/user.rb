53a9122257f6f633e30341992d16b70f187da974
shin224224 <sasasaeshin@gmail.com>
2021-07-24 11:17:05 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,18 +11,19 @@ class User < ApplicationRecord
     #  has_many : items
     #  has_many : item_purchases
 
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-    # memo confirmationはdeviseであるため記載の必要なし　
-
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
       # メモのため残します。空を許さない      with_optionsでvarlidationをまとめる , message: '全角文字を使用してください'
       validates :first_name_zenkaku
       validates :last_name_zenkaku
     end
 
+
     with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
       validates :first_name_kana_zenkaku
       validates :last_name_kana_zenkaku
     end
   end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    # memo confirmationはdeviseであるため記載の必要なし
+    #deviseでemailとpasswoedにはpresensetrueが最初からかかっている
 end
